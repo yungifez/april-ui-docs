b72b5898103d049f922979aa48a22cae11bcac53
Marvellous Ifezue <ifezueyoung@gmail.com>
2026-09-01 22:58:27 -0700

    fix: remove Font Awesome stylesheet
    Remove the unused Font Awesome CDN stylesheet from the docs layout.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -352,7 +352,6 @@
     @vite('resources/css/app.css')
     @stack('head-scripts')
     <livewire:styles />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 
 <body class="scroll-smooth">
